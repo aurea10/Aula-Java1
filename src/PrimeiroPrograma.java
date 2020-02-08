34b491fd28c24bd535e5ba07ff529cf564262e33
aurea10 <srta_ferr@hotmail.com>
2020-02-07 22:31:10 -0300

    Aula de java 1

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		String nome = "Rogerio";
+		String sobrenome = "de Morais";
+		String teste= "teste";
+
+		System.out.println(String.format("Olá, %s %s %s",nome,sobrenome,teste));
+
+	}
+
+}
